8a02ea7ad94490fe047f0758b6fd09275afc1ad7
Wenley Tong <wenley.tong@gmail.com>
2020-10-22 17:48:06 -0700

    Provide starter code for Ruby

--- lib/conway_game.rb
+++ lib/conway_game.rb
@@ -1,2 +1,10 @@
-class ConwayGame
+module ConwayGame
+  # Feel free to decide what representation you want!
+  def self.step(cells)
+    cells
+  end
+
+  def self.convert_to_string(cells)
+    ""
+  end
 end
--- spec/conway_game_spec.rb
+++ spec/conway_game_spec.rb
@@ -2,4 +2,43 @@ require 'spec_helper'
 require 'conway_game'
 
 RSpec.describe ConwayGame do
+  context 'empty cells' do
+    it 'can initialize' do
+      # Feel free to change the input type!
+      expect { ConwayGame.step([]) }.to_not raise_error
+    end
+  end
+
+  context 'square of cells' do
+    it 'stays alive' do
+      # Feel free to change the input type!
+      t0 = [
+        [1, 1],
+        [1, 1],
+      ]
+      t1 = ConwayGame.step(t0)
+
+      # somehow test that the square of cells is still alive
+      expect(t1).to eq(t0)
+    end
+  end
+
+  context 'blinker bar' do
+    it 'blinks' do
+      # Feel free to change the input type!
+      t0 = [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+      ]
+
+      t1 = ConwayGame.step(t0)
+      # Somehow test that the 3 cells "blinked" to be vertical
+      expect(t1).to eq([])
+
+      t2 = ConwayGame.step(t1)
+      # Expect the cells "blink back" to the starting positions
+      expect(t2).to eq(t0)
+    end
+  end
 end
